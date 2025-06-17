3521b476cd141201fc144c901b5a2f3600f07771
BBB1gB0ss <marcosb3b3@gmail.com>
2025-06-17 05:03:56 -0400

    update: Se cambio a mapa vectorial mediante maptiler

--- src/pages/Map/osm-providers.js
+++ src/pages/Map/osm-providers.js
@@ -1,6 +1,6 @@
 export default {
   maptiler: {
-    url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    url: "https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=XsuzFxIwtTsPtj9GLID4",
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   },
